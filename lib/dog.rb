ada4ba206b3e9ed81813ddae5d0aa4132f86fbd0
Sean Langton <slangton1@gmail.com>
2019-01-07 02:31:51 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -87,6 +87,7 @@ class Dog
 
   def self.find_by_name(name)
     dog_data = DB[:conn].execute("SELECT * FROM dogs WHERE name = ?", name)
+    binding.pry
     dog = Dog.all.find {|doggy| doggy.id == dog_data[0]}
     dog
   end
