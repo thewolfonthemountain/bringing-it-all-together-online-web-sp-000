24896a744040d8409d88de688f9c4089d9b62ad0
Sean Langton <slangton1@gmail.com>
2019-01-07 01:53:29 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,10 +3,13 @@ class Dog
   attr_accessor :name, :breed
   attr_reader :id
 
+  @@all = []
+  
   def initialize(id: nil, name:, breed:)
     @id = id
     @name = name
     @breed = breed
+    
   end
 
   def self.create_table
@@ -70,6 +73,7 @@ class Dog
   def self.find_or_create_by(name:, breed:)
     dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
     if !dog.empty?
+      binding.pry
       dog_data = dog[0]
       dog = Dog.new(dog_data[0], dog_data[1], dog_data[2])
     else
