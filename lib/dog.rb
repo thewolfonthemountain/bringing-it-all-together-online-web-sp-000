3ec5a1179a4d5ae02b2f0bca22db4f9b44846939
Sean Langton <slangton1@gmail.com>
2019-01-07 02:30:04 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -86,7 +86,7 @@ class Dog
   end
 
   def self.find_by_name(name)
-    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ?, name)
+    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ?, name")
   end
 
   def update
