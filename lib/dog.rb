670ee8ff7170cd5586a16d3e8ecfeebb00593e18
Sean Langton <slangton1@gmail.com>
2019-01-07 02:10:51 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -79,7 +79,7 @@ class Dog
     if !dog.empty?
       dog_data = dog[0]
       dog = Dog.all.find {|dog| dog.name == dog_data[1]}
-      if found_dog.nil?
+      if dog.nil?
         dog = Dog.new(dog_data[0], dog_data[1], dog_data[2])
       else
         dog
