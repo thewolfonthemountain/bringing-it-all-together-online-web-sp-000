1c29a9f939f16a11631ff3321d7c481f1ff6fbdd
Sean Langton <slangton1@gmail.com>
2019-01-07 01:53:36 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,12 +4,12 @@ class Dog
   attr_reader :id
 
   @@all = []
-  
+
   def initialize(id: nil, name:, breed:)
     @id = id
     @name = name
     @breed = breed
-    
+
   end
 
   def self.create_table
