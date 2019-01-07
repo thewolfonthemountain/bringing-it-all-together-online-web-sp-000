fc76636835e5d7e0e5f2ce2a1decc458b50c4556
Sean Langton <slangton1@gmail.com>
2019-01-07 02:19:35 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -46,7 +46,7 @@ class Dog
       SQL
       DB[:conn].execute(sql, self.name, self.breed)
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
-#      end
+      end
       self
     end
 
